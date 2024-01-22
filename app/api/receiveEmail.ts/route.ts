24d54e9c5c61647840c1d85afebcfd7e06d71ddd
Nouman <noumankhan95@yahoo.com>
2024-01-22 16:07:41 +0500

    api receive test

--- app/api/receiveEmail.ts/route.ts
+++ app/api/receiveEmail.ts/route.ts
@@ -3,19 +3,30 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
   try {
     const data = await req.formData();
-    const response = data.get("response");
+    const r = data.get("response");
     // const text = data.get("text");
-    await fetch(
+    console.log("User response From EMail ", r);
+    const res = await fetch(
       "https://emailmodo-2664b-default-rtdb.firebaseio.com/respnse.json",
       {
         body: JSON.stringify({
-          subject: response,
+          subject: r,
         }),
         method: "POST",
       },
     );
-    return NextResponse.json({ status: 1, data: { message: "Success" } });
+    return NextResponse.json(
+      {
+        status: 1,
+        data: { message: "Success" },
+      },
+      { status: 200 },
+    );
   } catch (e) {
-    return NextResponse.json({ status: 0, data: { message: e } });
+    console.log(e, "Error inreceive Email");
+    return NextResponse.json(
+      { status: 0, data: { message: e } },
+      { status: 403 },
+    );
   }
 }
