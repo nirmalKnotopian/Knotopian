6eaf4ef3374bbb572c3e88cdb5960213cac03c75
Nouman <noumankhan95@yahoo.com>
2024-03-08 03:24:39 +0500

    changes

--- app/api/sendsmtpemail/route.ts
+++ app/api/sendsmtpemail/route.ts
@@ -40,7 +40,7 @@ export async function POST(req: NextRequest) {
       createdAt: serverTimestamp(),
     });
     const emails = receps?.map(async (r: string) => {
-      const clientChoiceEmail = `<!DOCTYPE html>
+      const AmpHTML = `<!DOCTYPE html>
       <html ⚡4email data-css-strict>
         <head>
           <meta charset="utf-8" />
@@ -80,13 +80,13 @@ export async function POST(req: NextRequest) {
               border-radius: 50%;
               outline: none;
               cursor: pointer;
-                background-color:blue;
+              background-color:blue;
             }
             
-            /* Customize the appearance of the checked state */
+        
             input[type="radio"]:checked {
-              background-color: yellow; /* Change to your desired color */
-              border-color: blue; /* Change to your desired color */
+              background-color: yellow; 
+              border-color: blue; 
             }
 
           </style>
@@ -123,6 +123,8 @@ export async function POST(req: NextRequest) {
           <p style="font-weight:500;letter-spacing:0.4px;">
               We are a fastest growing eLearning production company with innovation as our core value. We design and develop custom learning solutions for the organizations, educational institutions, and NGOs. Our team is passionate about designing innovative eLearning strategies and solutions that concentrate on enhancing the workforce’s skills, attitudes, and efficiency and thereby producing measurable results with return on investment.
             </p>
+            <hr/>
+
             <br/>
     <input type="hidden" name="emailId" id="emailId" value="${emailId?.toString()}" />
     <input type="hidden" name="uEmail" id="uEmail" value="${r?.toString()}" />
@@ -132,10 +134,10 @@ export async function POST(req: NextRequest) {
         ${firstQuestion}
       </label>
       <br />
-      <label for="q1yes" style="display:flex;align-items:center;">
-        <input type="radio" id="q1yes" style="background-color: #007bff;color:#007bff" name="question1" value="yes"  on="change:AMP.setState({questionsState: {showLink:true} }),myform.submit"> Yes
+      <label for="q1yes" style="display:flex;align-items:center;margin-top:4px;margin-bottom:4px;">
+        <input type="radio" id="q1yes"  name="question1" value="yes"  on="change:AMP.setState({questionsState: {showLink:true} }),myform.submit"> Yes
       </label>
-      <label for="q1no" style="display:flex;align-items:center;">
+      <label for="q1no" style="display:flex;align-items:center;margin-top:4px;margin-bottom:4px;">
         <input type="radio" id="q1no" name="question1" value="no"  on="change:AMP.setState({questionsState: {selectedQuestion: 2,showLink:false}}),myform.submit"> No
       </label>
     </div>
@@ -147,10 +149,10 @@ export async function POST(req: NextRequest) {
         ${q}
       </label>
       <br />
-      <label for="q${index + 2}yes">
-        <input type="radio" id="q${index + 2}yes" style="background-color: #007bff;color:#007bff" name="question${index + 2}"  value="yes" on="change:AMP.setState({questionsState: {showLink:true} }),myform.submit"> Yes
+      <label for="q${index + 2}yes" style="display:flex;align-items:center;margin-top:4px;margin-bottom:4px;">
+        <input type="radio" id="q${index + 2}yes"  name="question${index + 2}"  value="yes" on="change:AMP.setState({questionsState: {showLink:true} }),myform.submit"> Yes
       </label>
-      <label for="q${index + 2}no">
+      <label for="q${index + 2}no" style="display:flex;align-items:center;margin-top:4px;margin-bottom:4px;">
         <input type="radio" id="q${index + 2}no" name="question${index + 2}"  value="no" on="change:AMP.setState({questionsState: {selectedQuestion: ${index + 3},showLink:false} }),myform.submit"> No
       </label>
     </div>`
@@ -160,8 +162,8 @@ export async function POST(req: NextRequest) {
        <h1 style="font-size: 18px; margin-bottom: 10px; color: #333">
         Are You Still Interested In Proceeding? 
        </h1>
-       <input type="radio" id="considerLater" on="change:myform.submit"  name="stillInterested" value="Consider Later"    > Consider Later
-       <input type="radio" id="Not Interested"  name="stillInterested" on="change:myform.submit" value="Not Interested Anymore"   > Not Interested Anymore     
+       <input type="radio" id="considerLater" on="change:myform.submit"  name="stillInterested" value="Consider Later"     > Consider Later
+       <input type="radio" id="NotInterested"  name="stillInterested" on="change:myform.submit" value="Not Interested Anymore"   > Not Interested Anymore     
      </label>  
             <button 
                      style="
@@ -183,6 +185,7 @@ export async function POST(req: NextRequest) {
               Use the calendar to set up a quick sync-up at your convenience to discuss further.
             </p>
            <br/> <button
+           hidden
                         style="
                     background-color: green;
                     color: #fff;
@@ -213,7 +216,189 @@ export async function POST(req: NextRequest) {
           </form>
         </body>
       </html>`;
+      const plainHTML = `<!DOCTYPE html>
+       <html >
+         <head>
+           <meta charset="utf-8" />
+           <style>
+             body {
+               visibility: hidden;
+             }
+           </style>
+           <style amp-custom>
+             /* any custom styles go here. */
+             
+             .hide {
+               display: none;
+             }
+             .show {
+               display: block;
+             }
+             input[type="radio"] {
+               -webkit-appearance: none;
+               -moz-appearance: none;
+               appearance: none;
+               width: 16px;
+               height: 16px;
+               border: 2px solid #333;
+               border-radius: 50%;
+               outline: none;
+               cursor: pointer;
+               background-color:blue;
+             }
+             
+         
+             input[type="radio"]:checked {
+               background-color: yellow; 
+               border-color: blue; 
+             }
+ 
+           </style>
+         </head>
+         <body style="
+               padding : 20px 10px; 
+                      background-color:blue;
+                      ">
+     
+           
+           <form
+             action="https://emailmodo.vercel.app/api/receiveEmail"
+             method="POST"
+                       style="
+                   max-width: 600px;
+                   margin: 0 auto;
+                   background-color: #fff;
+                   padding: 20px;
+                   border-radius: 8px;
+                   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                 "
+               id="myform"
+           >
+                <img  alt="logo"  width="120" 
+         height="30" style="object-fit:contain;background-color:#d3d3d3" src="https://knotopian.com/wp-content/uploads/2022/05/3.png" />
+               
+           <p style="font-weight:500;letter-spacing:0.4px;">
+               We are a fastest growing eLearning production company with innovation as our core value. We design and develop custom learning solutions for the organizations, educational institutions, and NGOs. Our team is passionate about designing innovative eLearning strategies and solutions that concentrate on enhancing the workforce’s skills, attitudes, and efficiency and thereby producing measurable results with return on investment.
+             </p>
+             <br/>
+     <input type="hidden" name="emailId" id="emailId" value="${emailId?.toString()}" />
+     <input type="hidden" name="uEmail" id="uEmail" value="${r?.toString()}" />
+     <input type="hidden" name="totalquestions" id="totalquestions" value="${EmailQuestions?.length.toString()}" />
+     <div style="margin-bottom: 20px">
+       <label style="font-size: 18px; margin-bottom: 10px; color: #333;font-weight:600;">
+         ${firstQuestion}
+       </label>
+       <label for="q1yes" style="display:flex;align-items:center;margin-top:4px;margin-bottom:4px;">
+       <input type="radio" id="q1yes"  name="question1" value="yes" > Yes
+      </label>
+         <label for="q1no" style="display:flex;align-items:center;margin-top:4px;margin-bottom:4px;">
+       <input type="radio" id="q1no" name="question1" value="no" > No
+     </label>
+     <h5 style="font-size: 14px; color: #555; margin-bottom: 15px">
+     If Yes Use the calendar to set up a quick sync-up at your convenience to
+     discuss further.
+      </h5>
 
+     <button 
+     style="
+    background-color: #E4D00A;
+    color: #fff;
+    border: none;
+    padding: 8px 20px;
+    font-size: 16px;
+    cursor: pointer;
+    border-radius: 4px;
+    margin-top:10px;
+  "
+    >
+<a    href="https://calendly.com/noumansajid95/meeting-test" target="_blank" style="text-decoration:none;color:#fff" >Schedule A Meeting</a>
+
+</button>
+     <br />
+
+       <br />
+      
+   
+     </div>
+        ${RestOfQuestion?.reduce((acc, q, index) => {
+          return (
+            acc +
+            `<div style="margin-bottom: 20px">
+                      <label style="font-size: 18px; margin-bottom: 10px; color: #333;font-weight:600;">
+                    ${q}
+                  </label>
+                  <br />
+                  <label for="q${index + 2}yes" style="display:flex;align-items:center;margin-top:4px;margin-bottom:4px;">
+                  <input type="radio" id="q${index + 2}yes"  name="question${index + 2}"  value="yes"> Yes
+                </label>
+                <label for="q${index + 2}no" style="display:flex;align-items:center;margin-top:4px;margin-bottom:4px;">
+                  <input type="radio" id="q${index + 2}no" name="question${index + 2}"  value="no"> No
+                </label>
+                  <h5 style="font-size: 14px; color: #555; margin-bottom: 15px">
+                  If Yes Use the calendar to set up a quick sync-up at your convenience to
+                  discuss further.
+                    </h5>
+      <button 
+                    style="
+                   background-color: #E4D00A;
+                   color: #fff;
+                   border: none;
+                   padding: 8px 20px;
+                   font-size: 16px;
+                   cursor: pointer;
+                   border-radius: 4px;
+                   margin-top:10px;
+                 "
+                   >
+           <a    href="https://calendly.com/noumansajid95/meeting-test" target="_blank" style="text-decoration:none;color:#fff" >Schedule A Meeting</a>
+             
+           </button>
+           
+                  
+                </div>`
+          );
+        }, "")}     
+        <label for="stillInterested" style="margin-bottom: 20px">
+        <h1 style="font-size: 18px; margin-bottom: 10px; color: #333">
+         Are You Still Interested In Proceeding? 
+        </h1>
+        <input type="radio" id="considerLater"  name="stillInterested" value="Consider Later"    > Consider Later
+        <input type="radio" id="NotInterested"  name="stillInterested" value="Not Interested Anymore"   > Not Interested Anymore     
+         </label>  
+         <br/>
+             <button 
+                      style="
+                     background-color: #E4D00A;
+                     color: #fff;
+                     border: none;
+                     padding: 8px 20px;
+                     font-size: 16px;
+                     cursor: pointer;
+                     border-radius: 4px;
+                     margin-top:10px;
+                   "
+                     >
+             <a    href="https://calendly.com/noumansajid95/meeting-test" target="_blank" style="text-decoration:none;color:#fff" >Schedule A Meeting</a>
+               
+             </button>
+             <p>
+               Use the calendar to set up a quick sync-up at your convenience to discuss further.
+             </p>
+            <br/> <button
+                         style="
+                     background-color: green;
+                     color: #fff;
+                     border: none;
+                     padding: 10px 20px;
+                     font-size: 16px;
+                     cursor: pointer;
+                     border-radius: 4px;
+                     margin-top:10px;
+                   "
+                     type="submit">Submit</button>
+           </form>
+         </body>
+       </html>`;
       await transporter.sendMail({
         from: "emailmodo@gmail.com", // sender address
         to: r, // list of receivers
@@ -221,8 +406,8 @@ export async function POST(req: NextRequest) {
         text:
           text ||
           "There is a new article. It's about sending emails, check it out!", // plain text body
-        amp: clientChoiceEmail, // html body
-        html: "<p>For clients that do not support AMP4EMAIL or amp content is not valid</p>",
+        amp: AmpHTML, // html body
+        html: plainHTML,
       });
     });
     await Promise.all(emails);
